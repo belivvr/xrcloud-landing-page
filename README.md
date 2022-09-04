a2318b71ee3e40bf613eec07c28a02062fd923e7
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-09-05 00:17:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">@xrcloud/landing-page</h1>
+
+<div align="center">
+  <img src="https://avatars.githubusercontent.com/u/40684200?s=200&v=4" alt="Belivvr Logo" />
+</div>
+
+<br>
+
+<div align="center">
+  <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" alt="TypeScript" />
+  <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React" />
+  <img src="https://img.shields.io/badge/eslint-3A33D1?style=for-the-badge&logo=eslint&logoColor=white" alt="ESLint" />
+</div>
+
+<br>
+
+<div align="center">
+  <img src="https://codecov.io/gh/belivvr/xrcloud-landing-page/branch/main/graph/badge.svg?token=5UR4QCATLZ" alt="codecov" />
+</div>
+
+---
+
+## Description
+
+XRCloud Landing Page
---
+++ jest.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/lib/**'
+  ],
+  testMatch: [
+    '**/?(*.)+(spec|test).ts?(x)'
+  ],
+  setupFilesAfterEnv: [
+    './jest.setup.ts',
+    'jest-plugin-context/setup'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/lib/',
+    '<rootDir>/types/'
+  ],
+  transform: {
+    '^.+\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-typescript'
+        ]
+      }
+    ]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/lib/',
+    '.storybook',
+
+    '/__snapshots__/'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/lib/',
+    '.storybook',
+
+    '.js',
+    '.interfaces.',
+    '.types.',
+    '.stories.',
+    '/__snapshots__/'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+}
